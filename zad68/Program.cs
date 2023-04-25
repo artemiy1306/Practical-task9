3e2d57108bd9a8cd9031110f7abac15a10545efe
artemiy <karelskiy@mail.ru>
2023-04-25 09:59:51 +0300

    add

--- zad68/Program.cs
+++ zad68/Program.cs
@@ -2,7 +2,7 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 //           | n + 1,                  m = 0
-// А(m, n) = | А(m - 1, 1),m > 0       n = О
+// А(m, n) = | А(m - 1, 1)             m > 0, n = О
 //           |А(m — 1, A(m, n — 1))    m > 0, n > О
 Console.Write("Введите число M: ");
 int m = Convert.ToInt32(Console.ReadLine());
@@ -22,34 +22,3 @@ int FunctionAkkerman(int m, int n)
     else
         return FunctionAkkerman(m - 1, FunctionAkkerman(m, n - 1));
 }
-
-
-
-
-
-
-// AkkermanFunction(m,n);
-
-
-// // вызов функции Аккермана
-// void AkkermanFunction(int m, int n)
-// {
-//     Console.Write(Akkerman(m, n)); 
-// }
-
-// // функция Аккермана
-// int Akkerman(int m, int n)
-// {
-//     if (m == 0)
-//     {
-//         return n + 1;
-//     }
-//     else if (n == 0 && m > 0)
-//     {
-//         return Akkerman(m - 1, 1);
-//     }
-//     else
-//     {
-//         return (Akkerman(m - 1, Akkerman(m, n - 1)));
-//     }
-// }
