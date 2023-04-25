1c7c70ece7824b7a90043c7bdc76185455c79907
artemiy <karelskiy@mail.ru>
2023-04-25 09:48:39 +0300

    add zad64

---
+++ zad64/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 5 N = 10 -> "5, 6, 7, 8, 9, 10"
+
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+GetNumbers(m, n);
+
+void GetNumbers(int m, int n)
+{
+  if (m > n) return;
+  else
+  {
+    Console.Write($"{m} , ");
+    GetNumbers(m+1 , n);
+    
+  }
+}
